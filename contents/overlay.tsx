607e48eaa7305489ac02dc0705893c7d6a2185da
vedran <vedran.prpic1@gmail.com>
2025-04-26 09:39:45 +0200

    remove createClient for supabse

--- contents/overlay.tsx
+++ contents/overlay.tsx
@@ -5,7 +5,7 @@ import { OverlayTopLeft, OverlayTopRight, OverlayBottomLeft, OverlayBottomRight 
 
 const SUPABASE_URL = "'http://127.0.0.1:54321"
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+// const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 const container = document.createElement("div")
 container.id = "plasmo-overlays-container"
